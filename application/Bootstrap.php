01ee385a3c85980bc3087d591d8d43054e6a74ab
Junior Moraes <juniorfvox@gmail.com>
2016-04-10 16:51:24 -0300

    configuração da controller global, que 'hookará' todas as requests e, futuramente, armazenará no MongoDB.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,6 +2,17 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+    public function _initGlobalPlugin() {
+        $this->bootstrap('frontController');
+        
+        require_once 'controllers/GlobalControllerPlugin.php'; 
+        $plugin = new GlobalControllerPlugin();
+
+        $front = Zend_Controller_Front::getInstance();
+        $front->registerPlugin($plugin);
+        
+        return $plugin;
+    }
 
 
 }
---
+++ application/controllers/GlobalControllerPlugin.php
@@ -0,0 +1,17 @@
+<?php
+
+class GlobalControllerPlugin extends Zend_Controller_Plugin_Abstract {
+    
+    public function preDispatch() {
+        $ipAddr   = $this->getRequest()->getServer('REMOTE_ADDR');
+        $protocol = $this->getRequest()->getServer('REQUEST_SCHEME');
+        $method   = $this->getRequest()->getServer('REQUEST_METHOD');
+        $uri      = Zend_Controller_Front::getInstance()->getRequest()->getRequestUri();
+
+        $request = new Application_Model_Request();
+        $request->addRequest($uri, $ipAddr, $protocol, $method);
+        //print_r($this->getRequest());
+    }
+}
+
+?>
