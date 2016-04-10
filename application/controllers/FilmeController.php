e0e79e44aa436a330b926cb92b4be6ad43aa9bdb
Junior Moraes <juniorfvox@gmail.com>
2016-04-10 18:58:19 -0300

    implementando edição e deleção de Filme.

--- application/controllers/FilmeController.php
+++ application/controllers/FilmeController.php
@@ -33,7 +33,7 @@ class FilmeController extends Zend_Controller_Action
                 $upload = new Zend_File_Transfer();
                 $files = $upload->getFileInfo();
               
-                $poster = file_get_contents($files['logo']['tmp_name']);
+                $poster = file_get_contents($files['poster']['tmp_name']);
 
                 $filme = new Application_Model_DbTable_Filme();
                 $filme->addFilme($nome, $produtora_id, $genero_id, $ano, $sinopse, $poster);
@@ -46,8 +46,91 @@ class FilmeController extends Zend_Controller_Action
         }
     }
 
+    public function posterAction()
+    {
+        $filme = new Application_Model_DbTable_Filme();
+        
+        $id = (int) $this->getRequest()->getParam('id');
+        
+        $row = $filme->fetchRow(array('id = ?' => $id))->toArray();
+
+        if (!$row) {
+            exit();
+        }
+        
+        $poster = $row['poster'];
+        
+        header("Content-type: image");
+        echo $poster;
+        
+        exit();
+
+    }
+
+    public function editAction()
+    {
+        $form = new Application_Form_Filme();
+        $form->submit->setLabel('Salvar');
+        
+        $this->view->form = $form;
+        
+        $filme = new Application_Model_DbTable_Filme();
+        
+        if ($this->getRequest()->isPost()) {
+            $formData = $this->getRequest()->getPost();
+            
+            if ($form->isValid($formData)) {
+                $id = (int) $form->getValue('id');
+                
+                $nome = $form->getValue('nome');
+                $produtora_id = $form->getValue('produtora_id');
+                $genero_id = $form->getValue('genero_id');
+                $ano = $form->getValue('ano');
+                $sinopse = $form->getValue('sinopse');
+                
+                $upload = new Zend_File_Transfer();
+                $files = $upload->getFileInfo();
+              
+                $poster = file_get_contents($files['poster']['tmp_name']);
+             
+                $filme->updateFilme($id, $nome, $produtora_id, $genero_id, $ano, $sinopse, $poster);
+                
+                $this->_helper->redirector('index');
+            }
+        }
+        else {
+            $id = $this->_getParam('id', 0);
+            
+            if ($id > 0) {
+                $data = $filme->fetchRow(array('id = ?' => $id))->toArray();
+                $form->populate($data);
+            }
+        }
+    }
+
+    public function deleteAction()
+    {
+        $filme = new Application_Model_DbTable_Filme();
+        
+        if ($this->getRequest()->isPost()) {
+            $id = $this->getRequest()->getPost('id');
+    
+            $filme->deleteFilme($id);
+        }
+
+        $this->_helper->redirector('index');
+        
+        return ;
+    }
+
 
 }
+
+
+
+
+
+
 
 
 
--- application/models/DbTable/Filme.php
+++ application/models/DbTable/Filme.php
@@ -17,6 +17,23 @@ class Application_Model_DbTable_Filme extends Zend_Db_Table_Abstract
 
         return $this->insert($data);
     }
+    
+    public function updateFilme($id, $nome, $produtora_id, $genero_id, $ano, $sinopse, $poster) {
+        $data = array(
+            'nome'         => $nome,
+            'produtora_id' => $produtora_id,
+            'genero_id'    => $genero_id,
+            'ano'          => $ano,
+            'sinopse'      => $sinopse,
+            'poster'       => $poster,
+        );
+        
+        return $this->update($data, array('id = ?' => (int) $id));
+    }
+    
+    public function deleteFilme($id) {        
+        $this->delete(array('id = ?' => (int) $id));
+    }
 
 }
 
