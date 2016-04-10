66c4cc193d723412c7634fb72e4382ae745e52f0
Junior Moraes <juniorfvox@gmail.com>
2016-04-10 18:29:40 -0300

    listagem de requests.

--- application/controllers/AcessoController.php
+++ application/controllers/AcessoController.php
@@ -10,7 +10,21 @@ class AcessoController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+        $order   = $this->_request->getQuery('order');
+        $orderBy = $this->_request->getQuery('orderBy');
+
+        $request = new Application_Model_Request();
+        
+        $rows = $request->getRequests($orderBy, $order);
+        
+        $resultSet = array();
+        foreach ($rows as $r) {
+            $r['accessDate'] = date('Y-m-d H:i:s', $r['accessDate']->sec);
+
+            $resultSet[] = $r;
+        }
+        
+        $this->view->requests = $resultSet;
     }
 
 
